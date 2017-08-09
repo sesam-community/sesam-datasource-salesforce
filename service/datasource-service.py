3dd7a54b086d66432b3fd0fc3978a2379f76c15d
Axel Borge <axel.borge@bouvet.no>
2017-08-09 15:58:21 +0200

    Add correct code

--- service/datasource-service.py
+++ service/datasource-service.py
@@ -1,100 +1,196 @@
+from functools import wraps
 from flask import Flask, request, Response, abort
 from datetime import datetime, timedelta
-import urllib, xmltodict
+import os
+
 import json
+import pytz
+from simple_salesforce import Salesforce
+import iso8601
+import logging
+from collections import OrderedDict
 
 app = Flask(__name__)
 
-cordinates = [{'x': 5, 'y': 59}, {'x': 5, 'y': 59.5}, {'x': 5, 'y': 60}, {'x': 5, 'y': 60.5}, {'x': 5, 'y': 61}, {'x': 5, 'y': 61.5}, {'x': 5, 'y': 62}, {'x': 5, 'y': 62.5}, {'x': 5.5, 'y': 59}, {'x': 5.5, 'y': 59.5}, {'x': 5.5, 'y': 60}, {'x': 5.5, 'y': 60.5}, {'x': 5.5, 'y': 61}, {'x': 5.5, 'y': 61.5}, {'x': 5.5, 'y': 62}, {'x': 5.5, 'y': 62.5}, {'x': 6, 'y': 59}, {'x': 6, 'y': 59.5}, {'x': 6, 'y': 60}, {'x': 6, 'y': 62.5}, {'x': 6, 'y': 63}, {'x': 6.5, 'y': 60.5}, {'x': 6.5, 'y': 61}, {'x': 6.5, 'y': 62.5}, {'x': 6.5, 'y': 63}, {'x': 7, 'y': 58}, {'x': 7, 'y': 60.5}, {'x': 7, 'y': 61}, {'x': 7, 'y': 63}, {'x': 7.5, 'y': 58}, {'x': 7.5, 'y': 61}, {'x': 7.5, 'y': 61.5}, {'x': 7.5, 'y': 62.5}, {'x': 7.5, 'y': 63}, {'x': 7.5, 'y': 63.5}, {'x': 8, 'y': 58}, {'x': 8, 'y': 61}, {'x': 8, 'y': 63}, {'x': 8, 'y': 63.5}, {'x': 8.5, 'y': 58}, {'x': 8.5, 'y': 63}, {'x': 8.5, 'y': 64}, {'x': 9, 'y': 58.5}, {'x': 9, 'y': 63.5}, {'x': 9, 'y': 64}, {'x': 9.5, 'y': 58.5}, {'x': 9.5, 'y': 64}, {'x': 9.5, 'y': 64.5}, {'x': 10, 'y': 59}, {'x': 10, 'y': 63.5}, {'x': 10, 'y': 64}, {'x': 10, 'y': 64.5}, {'x': 10.5, 'y': 59}, {'x': 10.5, 'y': 59.5}, {'x': 10.5, 'y': 63.5}, {'x': 10.5, 'y': 64.5}, {'x': 10.5, 'y': 65}, {'x': 11, 'y': 59}, {'x': 11, 'y': 64.5}, {'x': 11, 'y': 65}, {'x': 11, 'y': 65.5}, {'x': 11.5, 'y': 59}, {'x': 11.5, 'y': 64}, {'x': 11.5, 'y': 64.5}, {'x': 11.5, 'y': 65}, {'x': 11.5, 'y': 65.5}, {'x': 11.5, 'y': 66}, {'x': 11.5, 'y': 68}, {'x': 12, 'y': 65.5}, {'x': 12, 'y': 66}, {'x': 12, 'y': 66.5}, {'x': 12, 'y': 67}, {'x': 12, 'y': 67.5}, {'x': 12, 'y': 68}, {'x': 12.5, 'y': 65.5}, {'x': 12.5, 'y': 66}, {'x': 12.5, 'y': 66.5}, {'x': 12.5, 'y': 67}, {'x': 12.5, 'y': 67.5}, {'x': 12.5, 'y': 68}, {'x': 13, 'y': 66}, {'x': 13, 'y': 66.5}, {'x': 13, 'y': 67}, {'x': 13, 'y': 67.5}, {'x': 13, 'y': 68.5}, {'x': 13.5, 'y': 66.5}, {'x': 13.5, 'y': 67}, {'x': 13.5, 'y': 67.5}, {'x': 13.5, 'y': 68}, {'x': 13.5, 'y': 68.5}, {'x': 14, 'y': 67}, {'x': 14, 'y': 67.5}, {'x': 14, 'y': 68}, {'x': 14, 'y': 68.5}, {'x': 14, 'y': 69}, {'x': 14.5, 'y': 67.5}, {'x': 14.5, 'y': 68}, {'x': 14.5, 'y': 68.5}, {'x': 14.5, 'y': 69}, {'x': 15, 'y': 68}, {'x': 15, 'y': 68.5}, {'x': 15, 'y': 69}, {'x': 15, 'y': 69.5}, {'x': 15.5, 'y': 67.5}, {'x': 15.5, 'y': 69}, {'x': 15.5, 'y': 69.5}, {'x': 16, 'y': 69}, {'x': 16, 'y': 69.5}, {'x': 16, 'y': 70}, {'x': 16.5, 'y': 68}, {'x': 16.5, 'y': 68.5}, {'x': 16.5, 'y': 69}, {'x': 16.5, 'y': 69.5}, {'x': 16.5, 'y': 70}, {'x': 17, 'y': 68.5}, {'x': 17, 'y': 69}, {'x': 17, 'y': 69.5}, {'x': 17, 'y': 70}, {'x': 17, 'y': 70.5}, {'x': 17.5, 'y': 68.5}, {'x': 17.5, 'y': 69}, {'x': 17.5, 'y': 69.5}, {'x': 17.5, 'y': 70}, {'x': 17.5, 'y': 70.5}, {'x': 18, 'y': 69}, {'x': 18, 'y': 69.5}, {'x': 18, 'y': 70}, {'x': 18, 'y': 70.5}, {'x': 18.5, 'y': 69.5}, {'x': 18.5, 'y': 70}, {'x': 18.5, 'y': 70.5}, {'x': 19, 'y': 69.5}, {'x': 19, 'y': 70.5}, {'x': 19.5, 'y': 69.5}, {'x': 19.5, 'y': 70}, {'x': 19.5, 'y': 70.5}, {'x': 20, 'y': 69.5}, {'x': 20, 'y': 70}, {'x': 20, 'y': 70.5}, {'x': 20.5, 'y': 69.5}, {'x': 20.5, 'y': 70}, {'x': 20.5, 'y': 70.5}, {'x': 21, 'y': 69.5}, {'x': 21, 'y': 70}, {'x': 21, 'y': 70.5}, {'x': 21.5, 'y': 70}, {'x': 21.5, 'y': 70.5}, {'x': 22, 'y': 70}, {'x': 22, 'y': 70.5}, {'x': 22.5, 'y': 70}, {'x': 22.5, 'y': 70.5}, {'x': 23, 'y': 70}, {'x': 23, 'y': 70.5}, {'x': 23.5, 'y': 70}, {'x': 24, 'y': 70.5}, {'x': 25, 'y': 70.5}, {'x': 25.5, 'y': 70.5}, {'x': 26.5, 'y': 70.5}, {'x': 27, 'y': 70.5}, {'x': 27.5, 'y': 70.5}, {'x': 28, 'y': 70.5}, {'x': 28.5, 'y': 70.5}, {'x': 29, 'y': 70}, {'x': 29.5, 'y': 70}, {'x': 30, 'y': 70}, {'x': 30.5, 'y': 70}, {'x': 30.5, 'y': 70.5}]
-
-divfactor = 2
-
-startLong = 5 * divfactor
-endLong = 31 * divfactor
-startLat = 58 * divfactor
-endLat = 71 * divfactor
-
+logger = None
 
 class DataAccess:
     def __init__(self):
-        self._entities = {"stationinfo": [], "location": [], "highlow": []}
+        self._entities = {"Activity": [], "Contact": [], "Account": [], "Lead": [], "Task": [], "Event": [], "Group": [], "Opportunity": [], "User": [], "EventRelation": [], "Case": []}
 
-
-    def get_entities(self, since, datatype):
+    def get_entities(self, since, datatype, sf):
         if not datatype in self._entities:
             abort(404)
         if since is None:
-            return self.get_entitiesdata(datatype, since)
+            return self.get_entitiesdata(datatype, since, sf)
         else:
-            return [entity for entity in self.get_entitiesdata(datatype, since) if entity["_updated"] > since]
+            return [entity for entity in self.get_entitiesdata(datatype, since, sf) if entity["_updated"] > since]
 
-    def get_entitiesdata(self, datatype, since):
-        if datatype in self._entities:
-            if len(self._entities[datatype]) > 0 and self._entities[datatype][0]["_updated"] > "%sZ" % (datetime.now() - timedelta(hours=12)).isoformat():
-               return self._entities[datatype]
-        now = datetime.now()
-        start = since
-        if since is None:
-            start = (now - timedelta(days=365)).isoformat()
+    def get_entitiesdata(self, datatype, since, sf):
+        # if datatype in self._entities:
+        #     if len(self._entities[datatype]) > 0 and self._entities[datatype][0]["_updated"] > "%sZ" % (datetime.now() - timedelta(hours=12)).isoformat():
+        #        return self._entities[datatype]
+        now = datetime.now(pytz.UTC)
         entities = []
-        if datatype == "highlow":
-            for c in cordinates:
-                file = urllib.request.urlopen(
-                    "http://api.sehavniva.no/tideapi.php?lat=%s&lon=%s&fromtime=%sZ&totime=%sZ&datatype=tab&refcode=cd&place=&file=&lang=nn&interval=60&dst=0&tzone=0&tide_request=locationdata" % (
-                        c["y"], c["x"], start, (now + timedelta(days=31)).isoformat()))
-                data = file.read()
-                file.close()
+        end = datetime.now(pytz.UTC)  # we need to use UTC as salesforce API requires this
 
-                locationData = xmltodict.parse(data)["tide"]["locationdata"]
-                if "location" in locationData:
-                    for e in locationData["data"]["waterlevel"]:
-                        e["@time"] = e["@time"].replace("+00:00", "Z")
-                        e.update({"_id": "%s_%s_%s" % (c["y"] , c["x"], e["@time"])})
-                        e.update({"location_id": "%s_%s" % (c["y"] , c["x"])})
-                        e.update({"_updated": "%sZ" % now.isoformat()})
-                        e.update({"type": locationData["data"]["@type"]})
+        if since is None:
+            #fields = getattr(sf, datatype).describe()["fields"]
+            result = [x['Id'] for x in sf.query("SELECT Id FROM %s" % (datatype))["records"]]
+        else:
+            start = iso8601.parse_date(since)
+            if getattr(sf, datatype):
+                if end > (start + timedelta(seconds=60)):
+                    result = getattr(sf, datatype).updated(start, end)["ids"]
+                    deleted = getattr(sf, datatype).deleted(start, end)["deletedRecords"]
+                    for e in deleted:
+                        c = OrderedDict({"_id": e["id"]})
+                        # c = {k: v for k, v in c.items() if v}
+                        c.update({"_updated": "%s" % e["deletedDate"]})
+                        c.update({"_deleted": True})
 
-                        entities.append(e)
-                        
-        if datatype == "location":
-            for c in cordinates:
-                file = urllib.request.urlopen(
-                    "http://api.sehavniva.no/tideapi.php?lat=%s&lon=%s&refcode=cd&place=&file=&lang=en&tide_request=locationlevels" % (
-                    c["y"] , c["x"]))
-                data = file.read()
-                file.close()
+                        entities.append(c)
+        if result:
+            for e in result:
+                c = getattr(sf, datatype).get(e)
+               # c = {k: v for k, v in c.items() if v}
+                c.update({"_id": e})
+                c.update({"_updated": "%s" % c["LastModifiedDate"]})
 
-                locationData = xmltodict.parse(data)
-                if "tide" in locationData:
-                    locationData = xmltodict.parse(data)["tide"]["locationlevel"]
-                    if "location" in locationData:
-                        locationData.update({"_id": "%s_%s" % (c["y"] , c["x"])})
-                        locationData.update({"_updated": "%sZ" % now.isoformat()})
-
-                        entities.append(locationData)
-
-        if datatype == "stationinfo":
-            file = urllib.request.urlopen(
-                'http://api.sehavniva.no/tideapi.php?type=perm&tide_request=stationlist')
-            data = file.read()
-            file.close()
-
-            entities = xmltodict.parse(data)["tide"]["stationinfo"]["location"]
-            for e in entities:
-                e.update({"_id": e["@code"]})
-                e.update({"_updated": "%sZ" % now.isoformat()})
+                entities.append(c)
 
         self._entities[datatype] = entities
         return self._entities[datatype]
 
 data_access_layer = DataAccess()
 
-@app.route('/<datatype>')
+def get_var(var):
+    envvar = None
+    if var.upper() in os.environ:
+        envvar = os.environ[var.upper()]
+    else:
+        envvar = request.args.get(var)
+    logger.info("Setting %s = %s" % (var, envvar))
+    return envvar
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        auth = request.authorization
+        if not auth:
+            return authenticate()
+        return f(*args, **kwargs)
+
+    return decorated
+
+@app.route('/<datatype>', methods=['GET'])
+@requires_auth
 def get_entities(datatype):
     since = request.args.get('since')
-    entities = data_access_layer.get_entities(since, datatype)
+    instance = get_var('instance') or "prod"
+    use_sandbox = False
+    if instance == "sandbox":
+        use_sandbox = True
+        logger.info("Using sandbox")
+    auth = request.authorization
+    token, username = auth.username.split('\\', 1)
+    password = auth.password
+    sf = Salesforce(username, password, token, sandbox=use_sandbox)
+    entities = sorted(data_access_layer.get_entities(since, datatype, sf), key=lambda k: k["_updated"])
+
     return Response(json.dumps(entities), mimetype='application/json')
 
+
+
+@app.route('/<datatype>', methods=['POST'])
+@requires_auth
+def receiver(datatype):
+    # get entities from request and write each of them to a file
+    entities = request.get_json()
+    app.logger.info("Updating entity of type %s" % (datatype))
+    app.logger.debug(json.dumps(entities))
+    instance = get_var('instance') or "prod"
+    use_sandbox = False
+    if instance == "sandbox":
+        use_sandbox = True
+        logger.info("Using sandbox")
+    auth = request.authorization
+    token, username = auth.username.split('\\', 1)
+    sf = Salesforce(username, auth.password, token, sandbox=use_sandbox)
+
+    if getattr(sf, datatype):
+        transform(datatype, entities, sf)# create the response
+    return Response("Thanks!", mimetype='text/plain')
+
+def transform(datatype, entities, sf):
+    global ids
+    # create output directory
+    c = None
+    listing = []
+    if not isinstance(entities, (list)):
+        listing.append(entities)
+    else:
+        listing = entities
+    for e in listing:
+        app.logger.info("Updateing entity internal id %s of type %s" % (e["_id"], datatype))
+        del e["_id"]
+        if not ("_deleted" in e and e["_deleted"]):
+            if "Id" in e:
+                app.logger.debug("Getting entity %s of type %s" % (e["Id"], datatype))
+                c = getattr(sf, datatype).get(e["Id"])
+            if not c and "sesam_id__c" in e:
+                try:
+                    c = getattr(sf, datatype).get_by_custom_id("sesam_id__c", e["sesam_id__c"])
+                except:
+                    pass
+            if not c:
+                d = []
+                for p in e.keys():
+                    if p.startswith("_"):
+                        d.append(p)
+                for p in d:
+                    del(e[p])
+                if "Id" in e:
+                    del (e["Id"])
+                getattr(sf, datatype).create(e)
+        if "_deleted" in e and e["_deleted"] :
+            if "Id" in e:
+                app.logger.info("Deleting entity %s of type %s" % (e["Id"],datatype))
+                try:
+                    getattr(sf, datatype).delete(c["Id"])
+                except Exception as err:
+                    app.logger.info("Entity %s of type %s does not exist. Ignoring error: %s..." % (e["Id"], datatype, type(err)))
+                    pass
+        else:
+            d = []
+            for p in e.keys():
+                if p.startswith("_"):
+                    d.append(p)
+            for p in d:
+                del(e[p])
+            if "Id" in e:
+                del (e["Id"])
+            getattr(sf, datatype).update(c["Id"], e)
+
+
 if __name__ == '__main__':
+    # Set up logging
+    format_string = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logger = logging.getLogger('salesforce-microservice')
+
+    # Log to stdout
+    stdout_handler = logging.StreamHandler()
+    stdout_handler.setFormatter(logging.Formatter(format_string))
+    logger.addHandler(stdout_handler)
+
+    logger.setLevel(logging.DEBUG)
+
     app.run(debug=True, host='0.0.0.0')
 
